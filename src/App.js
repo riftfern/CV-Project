d63f552eff30cf10f683abfce8afd36a9a565795
riftfern <adriftfern@gmail.com>
2023-03-05 15:34:48 -0500

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
+import React, { Component } from "react";
+import Header from "./components/Header";
+import PersonalInfo from "./components/PersonalInfo";
+import FormalEducation from "./components/FormalEducation";
+import SelfEducation from "./components/SelfEducation";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import CvContent from "./components/CvContent";
 
+//import uniqid from 'uniqid';
 
-//export default App;
+class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <Header />
+                <PersonalInfo />
+                <FormalEducation />
+                <SelfEducation />
+                <Experience />
+                <Projects />
+                <CvContent />
+             </div>
+        )
+    }
+}
+
+export default App;
