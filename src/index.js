e33d4d345c02dedad4f1ac5ad8aceb5f12a27dc3
riftfern <adriftfern@gmail.com>
2023-03-05 15:35:02 -0500

    commit changes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import PersonalInfo from './components/PersonalInfo';
-import Header from './components/Header';
-import FormalEducation from './components/FormalEducation';
-import SelfEducation from './components/SelfEducation';
-import Experience from './components/Experience';
-import CvContent from './components/CvContent';
-import Projects from './components/Projects';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <PersonalInfo />
-    <FormalEducation />
-    <SelfEducation />
-    <Experience />
-    <Projects />
-    <CvContent /> 
+    <App />
   </React.StrictMode>
 );
 
