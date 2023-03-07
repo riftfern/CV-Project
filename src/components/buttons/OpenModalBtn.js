6478405a1b0191fb95bd788c6ba99e1d3ecf10ed
riftfern <adriftfern@gmail.com>
2023-03-06 19:30:56 -0500

    moved directories

---
+++ src/components/buttons/OpenModalBtn.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function OpenModalBtn(props) {
+  return (
+    <div className="modal-btn-container">
+      <button className="open-modal-btn" onClick={props.handleDisplay}>
+        + Add {props.title}
+      </button>
+    </div>
+  );
+}
+
+export default OpenModalBtn;
